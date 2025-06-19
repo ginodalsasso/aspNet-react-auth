d71d0c99a3e14223a26028db1c49dddaf0beca58
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-06-19 20:10:15 +0200

    Replace the TokenResponseDto deletion by RefreshTokenSimpleRequestDto.

--- aspNet-react-auth.Server/Models/RefreshTokenSimpleRequestDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace aspNet_react_auth.Server.Models
-{
-    public class RefreshTokenSimpleRequestDto
-    {
-        public Guid UserId { get; set; }
-    }
-}
---
+++ aspNet-react-auth.Server/Models/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace aspNet_react_auth.Server.Models
+{
+    public class TokenResponseDto
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
