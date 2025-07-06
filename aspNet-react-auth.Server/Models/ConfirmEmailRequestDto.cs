1ec163bdd97185c8e69ca689b356b0438d16bfc3
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-07-06 18:18:29 +0200

    NEW: server: ConfirmEmailRequestDto.

---
+++ aspNet-react-auth.Server/Models/ConfirmEmailRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspNet_react_auth.Server.Models
+{
+    public class ConfirmEmailRequestDto
+    {
+        [Required]
+        public required string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public required string Token { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; } = string.Empty;
+    }
+}
